eeb62308af26bb14f3cd83f0fe96b186b9302c6a
NinoFullStack <zapakh.pizdy@gmail.com>
2023-09-12 16:15:52 +0300

    counter up for home page

--- js/app.js
+++ js/app.js
@@ -19,3 +19,53 @@ const spotlightsTechnologiesSlider = new Swiper('.spotlights-technologies', {
     },
   },
 })
+
+const counterUp = (interval = 1) => {
+  const counterItems = document.querySelectorAll('[data-counter]')
+
+  counterItems.forEach(item => {
+    let startValue = 1
+    const endValue = parseInt(item.getAttribute('data-counter'))
+    const duration = Math.floor(interval / endValue)
+
+    let counter = setInterval(() => {
+      startValue += 1
+
+      if (endValue.toString().length >= 5) {
+        if (startValue <= 10000) {
+          startValue += 50
+        }
+
+        if (startValue >= 10000 && !(startValue >= 11000)) {
+          startValue += 5
+        }
+
+        if (startValue >= 11000) {
+          startValue += 1
+        }
+      }
+
+      item.textContent = startValue
+
+      if (startValue === endValue) clearInterval(counter)
+    }, duration)
+  })
+}
+
+const isViewCounter = () => {
+  const counters = document.querySelectorAll('[data-counter]')
+
+  counters.forEach(counter => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(item => {
+        if (item.isIntersecting) {
+          counterUp(1000)
+        }
+      })
+    })
+
+    observer.observe(counter)
+  })
+}
+
+isViewCounter()
